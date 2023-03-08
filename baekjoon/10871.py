264baaab9c8edcd30215d5e70e4671f496740add
cloer <chltjrrb97@gmail.com>
2023-03-08 13:59:38 +0900

    Add: baekjoon/10871.py

---
+++ baekjoon/10871.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/10871
+# 구현
+
+# My solution
+import sys
+Io = sys.stdin.readline
+n, x = map(int, Io().split())
+a = list(map(int, Io().rstrip().split()))
+
+result = []
+for i in a:
+    if i < x: result.append(i)
+print(*result)
