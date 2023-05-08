fd55557de169f54aa7bd866c9e32128679eb10d4
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-05-09 05:08:24 +0900

    [level 2] Title: 소수 찾기, Time: 15.21 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -24,10 +24,13 @@ class Solution {
     }
     
     public boolean isPrime(Integer number){
-        if(number == 0 || number == 1)
+        if(number == 2)
+            return true;
+        
+        if(number == 0 || number == 1 || number % 2 == 0)
             return false;
         
-        for(int i=2; i<=(int)Math.sqrt(number); i++)
+        for(int i=3; i<=(int)Math.sqrt(number); i+=2)
             if(number % i == 0)
                 return false;
         
