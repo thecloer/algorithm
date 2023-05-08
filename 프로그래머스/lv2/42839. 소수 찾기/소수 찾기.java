06358605b1577fa4315df8b5bf7a2c9f3a49630c
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-05-09 04:57:50 +0900

    [level 2] Title: 소수 찾기, Time: 12.80 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    HashSet<Integer> combs = new HashSet();
+    
+    public int solution(String numbers) {
+        int answer = 0;
+        combination("", numbers);
+        
+        Iterator<Integer> iter = combs.iterator();
+        while(iter.hasNext())
+            if(isPrime(iter.next()))
+                answer++;
+        
+        return answer;
+    }
+    
+    public void combination(String prev, String others){
+        if(!prev.equals(""))
+            combs.add(Integer.valueOf(prev));
+        
+        for(int i=0; i<others.length(); i++)
+            combination(prev+others.charAt(i), others.substring(0,i)+others.substring(i+1));
+    }
+    
+    public boolean isPrime(Integer number){
+        if(number == 0 || number == 1)
+            return false;
+        
+        for(int i=2; i<=(int)Math.sqrt(number); i++)
+            if(number % i == 0)
+                return false;
+        
+        return true;
+    }
+}
