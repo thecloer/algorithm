34ad2ba48121c3d61c2240601db579b62cd49dac
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-05-09 09:17:05 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.java
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -5,12 +5,11 @@ class Solution {
         int answer = 1;
         HashMap<String,Integer> map = new HashMap();
         
-        for(String[] cloth:clothes)         
-            map.put(cloth[1], map.getOrDefault(cloth[1],0) + 1);
+        for(String[] cloth: clothes)         
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         
         for(int value: map.values())
             answer *= value + 1;
-        
         
         return answer - 1;
     }
