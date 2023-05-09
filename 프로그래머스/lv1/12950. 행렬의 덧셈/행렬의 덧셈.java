a08479e66daa426a4bd4af649f5066aea2a3a786
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-05-09 14:58:56 +0900

    [level 1] Title: 행렬의 덧셈, Time: 3.32 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = arr1;
+        for(int i=0; i<answer.length; i++)
+            for(int j=0; j<answer[0].length; j++)
+                answer[i][j] += arr2[i][j];
+        return answer;
+    }
+}
