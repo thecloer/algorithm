a8941909c7109a458a15dd65c0301df8a00254a4
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-05-26 11:42:33 +0900

    [level 2] Title: [3차] n진수 게임, Time: 15.47 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        int turn = 0;
+        int num = 0;
+        game: while(true){
+            for(char c:Integer.toString(num++,n).toCharArray()){
+                if(turn++ % m == p - 1)
+                    answer+=Character.toUpperCase(c);
+                if(turn >= m*(t-1)+p) 
+                    break game;
+            }
+        }
+        return answer;
+    }
+}
