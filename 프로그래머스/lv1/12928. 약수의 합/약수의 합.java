176cf5f3bab2392b7bd41056efb4a2d1c6596fdb
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-06-04 19:38:55 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,8 +1,10 @@
 class Solution {
     public int solution(int n) {
-        int answer = n;
-        for(int i=1; i<n; i++)
-            if(n%i==0) answer+=i;
+        int answer = 0;
+        for(int i=1; i<=Math.sqrt(n); i++){
+            if(n%i!=0) continue;
+            answer+=i+(n/i==i?0:n/i);
+        }
         
         return answer;
     }
