b8090acb3e183092848be35fead947e8b3f113db
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-06-05 04:58:51 +0900

    [unrated] Title: 귤 고르기, Time: 68.11 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -4,12 +4,12 @@ class Solution {
         Map<Integer,Integer> map = new HashMap();
         for(int key:tangerine)
             map.put(key, map.getOrDefault(key,0)+1);
-        
-        Integer[] counts = map.values().stream().sorted(Collections.reverseOrder()).toArray(Integer[]::new);
+
+        Iterator<Integer> iter = map.values().stream().sorted(Collections.reverseOrder()).iterator();
         int sum =0;
         int answer = 0;
-        for(int count:counts){
-            sum+=count;
+        while(iter.hasNext()){
+            sum+=iter.next();
             answer++;
             if(sum>=k) break;
         }
