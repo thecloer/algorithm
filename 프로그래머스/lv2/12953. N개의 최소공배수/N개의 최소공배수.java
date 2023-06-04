fadf32e15c8c6eb0aba3ffc8031df56df4ee69bc
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-06-04 19:25:52 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 1;
+        for(int i:arr)
+            answer = LCM(answer,i);
+        
+        return answer;
+    }
+    int LCM(int a, int b){
+        return (a*b)/GCD(a,b);
+    }
+    int GCD(int a, int b){
+        if(a<b){
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        if(b==0) return a;
+        return GCD(b, a%b);
+    }
+}
