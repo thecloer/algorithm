83e39fa7bbed4f31f1f593e0c7cb15d1cb8452d6
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-06-06 16:58:51 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.23 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        Stack<Character> stack = new Stack();
+        char c;
+        rotate: for(int start=0; start<s.length(); start++){
+            for(int i=0; i<s.length(); i++){
+                c = s.charAt((start+i)%s.length());
+                if(c == '('|| c=='[' || c =='{') stack.push(c);
+                else if(c == ')'|| c==']' || c =='}') {
+                    if(stack.size() < 1) continue rotate;
+                    if(!isPair(stack.pop(),c)) continue rotate;
+                }
+            }
+            if(stack.size()!=0) continue rotate;
+            answer++;
+        }
+        return answer;
+    }
+    boolean isPair(char open, char close){
+        if(open=='('&&close==')') return true;
+        if(open=='['&&close==']') return true;
+        if(open=='{'&&close=='}') return true;
+        return false;
+    }
+}
