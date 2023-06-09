af21c0b9ca6115ece69ecf2d1699b573bcfff1fc
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-06-09 21:54:38 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(int x) {
+        int xCopy = x;
+        int sum = 0;
+        while(xCopy>0){
+            sum+=xCopy%10;
+            xCopy /= 10;
+        }
+        return (double)x/sum == x/sum;
+    }
+}
