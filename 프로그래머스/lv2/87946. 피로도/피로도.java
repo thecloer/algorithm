4912c0a0f64356ffec0c6f551d1ea9ba0c1467f0
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-06-09 11:39:29 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,17 +1,14 @@
 class Solution {
     public int solution(int k, int[][] dungeons) {
-        boolean[] visited = new boolean[dungeons.length];
-        return dfs(k, dungeons, visited);
+        return dfs(k, dungeons, new boolean[dungeons.length]);
     }
     int dfs(int energe, int[][] dungeons, boolean[] visited){
-        if(energe<0) return 0;
         int count = 0;
         for(int i=0; i<dungeons.length; i++){
-            if(!visited[i] && energe>=dungeons[i][0]){
-                visited[i] = true;
-                count = Math.max(count, dfs(energe-dungeons[i][1],dungeons,visited)+1);
-                visited[i] = false;
-            }
+            if(visited[i] || energe<dungeons[i][0]) continue;
+            visited[i] = true;
+            count = Math.max(count, dfs(energe-dungeons[i][1],dungeons,visited)+1);
+            visited[i] = false;
         }
         return count;
     }
