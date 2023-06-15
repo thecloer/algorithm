a131d54f71ea4de49b82b6c56214a3649e3a3a17
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-06-15 09:12:53 +0900

    [unrated] Title: 과일 장수, Time: 0.34 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -1,14 +1,10 @@
 import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
-        Integer[] arr = Arrays.stream(score)
-                            .boxed()
-                            .sorted(Collections.reverseOrder())
-                            .toArray(Integer[]::new);
-        
+        Arrays.sort(score);
         int answer = 0;
-        for(int i=m-1; i<arr.length; i+=m)
-            answer += arr[i]*m;
+        for(int i=score.length-m; i>=0; i-=m)
+            answer += score[i]*m;
         return answer;
     }
 }
