3be5143b2b8686f33688f5988fcddea785eedf67
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-06-18 16:04:36 +0900

    [level 2] Title: 땅따먹기, Time: 20.78 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    int solution(int[][] land) {
+        for(int row=1; row<land.length; row++){
+            for(int col=0; col<4; col++)
+                land[row][col] += maxWithout(land[row-1], col);
+        }
+        return max(land[land.length-1]);
+    }
+    
+    int max(int[] arr){
+        int m=0;
+        for(int i=0; i<arr.length; i++)
+            if(m<arr[i]) 
+                m = arr[i];
+        return m;
+    }
+    int maxWithout(int[] arr, int excludedIndex){
+        int m=0;
+        for(int i=0; i<arr.length; i++)
+            if(i!=excludedIndex && m<arr[i]) 
+                m = arr[i];
+        return m;
+    }
+}
