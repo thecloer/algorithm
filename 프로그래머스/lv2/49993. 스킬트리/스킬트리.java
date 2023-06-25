b63c2f895d11a67159350eceaa8a5894e9ec6af4
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-06-25 16:17:43 +0900

    [level 2] Title: 스킬트리, Time: 1.70 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -3,7 +3,8 @@ class Solution {
         int answer = 0;
         for(String tree:skill_trees){
             tree = tree.replaceAll(String.format("[^%s]",skill),"");
-            if(tree.length()==0||(tree.charAt(0)==skill.charAt(0)&&skill.contains(tree))) answer++;
+            if(skill.indexOf(tree) == 0)
+                answer++;
         }
         return answer;
     }
