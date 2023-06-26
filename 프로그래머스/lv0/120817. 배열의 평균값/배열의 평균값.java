b8f5ab9809af8fb08da66ad2c7265a81d90889fd
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-06-27 06:11:20 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0 ;
+        for(int n:numbers)
+            answer+=n;
+        return answer/numbers.length;
+    }
+}
