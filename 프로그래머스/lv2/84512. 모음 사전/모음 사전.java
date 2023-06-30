97487c3cfe5e55a9492ae712f7656fdbb9c33ffc
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-06-30 22:24:22 +0900

    [level 2] Title: 모음 사전, Time: 0.82 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,20 +1,19 @@
 class Solution {
     final String LETTERS = "AEIOU";
     public int solution(String word) {
-        int count =0;
         return dfs(word,0,new StringBuilder());
     }
     int dfs(String word, int count, StringBuilder sb){
-        for(int i=0; i<LETTERS.length(); i++){
-            sb.append(LETTERS.charAt(i));
-            count++;
-            if(word.equals(sb.toString())) 
-                break;
-            if(sb.length()<5)
-                count = dfs(word, count, sb);
-            if(word.equals(sb.toString())) 
-                break;
-            sb.deleteCharAt(sb.length()-1);
+        if(!word.equals(sb.toString())) {
+            for(int i=0; i<LETTERS.length(); i++){
+                sb.append(LETTERS.charAt(i));
+                count++;
+                if(sb.length()<5)
+                    count = dfs(word, count, sb);
+                if(word.equals(sb.toString())) 
+                    break;
+                sb.deleteCharAt(sb.length()-1);
+            }
         }
         return count;
     }
