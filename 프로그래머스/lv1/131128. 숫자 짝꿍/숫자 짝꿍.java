293b3c9df5e82793e91804dfb043f1e76c922037
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-07-01 07:55:57 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,29 +1,22 @@
 import java.util.*;
 class Solution {
     public String solution(String X, String Y) {
-        Map<Character, Integer> mapX = new HashMap();
-        Map<Character, Integer> mapY = new HashMap();
+        StringBuilder ans = new StringBuilder();
+        int[] x = new int[10];
+        int[] y = new int[10];
         for(char c:X.toCharArray())
-            mapX.put(c,mapX.getOrDefault(c,0)+1);
+            x[c-'0']++;
         for(char c:Y.toCharArray())
-            mapY.put(c,mapY.getOrDefault(c,0)+1);
+            y[c-'0']++;
         
-        Set<Character> inter = mapX.keySet();
-        inter.retainAll(mapY.keySet());
-        if(inter.size()==0)
+        for(int i=9; i>=0; i--)
+            for(int j=0, n=Math.min(x[i],y[i]); j<n; j++)
+                ans.append(i);
+        
+        if(ans.length()==0)
             return "-1";
-        if(inter.size()==1 && inter.contains('0'))
+        if(ans.charAt(0)=='0')
             return "0";
-        
-        Set<Character> iterSet = new TreeSet<>((c1,c2)->c2.compareTo(c1));
-        iterSet.addAll(inter);
-        Iterator<Character> iter = iterSet.iterator();
-        StringBuilder sb = new StringBuilder();
-        while(iter.hasNext()){
-            char c = iter.next();
-            for(int i=0, n=Math.min(mapX.get(c),mapY.get(c)); i<n; i++)
-                sb.append(c);
-        }
-        return sb.toString();
+        return ans.toString();
     }
 }
