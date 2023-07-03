8c03ed95863391a1361ad75d40fc2972c6b29b66
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-07-03 21:03:02 +0900

    [level 3] Title: 등굣길, Time: 0.48 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.java
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.java
@@ -1,27 +1,21 @@
-import java.util.*;
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] map = new int[n+1][m+1];
-        for(int i=0; i<puddles.length; i++)
-            map[puddles[i][1]][puddles[i][0]] = -1;
-        for(int y=1; y<=n; y++){
-            for(int x=1; x<=m; x++){
-                if(x==1 && y==1) {
-                    map[y][x]=1;
+        int[][] map = new int[m+1][n+1];
+        for(int[] p:puddles)
+            map[p[0]][p[1]] = -1;
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(i==1 && j==1){
+                    map[i][j]=1;
                     continue;
-                };
-                if(map[y][x]<0)
+                }
+                if(map[i][j]==-1){
+                    map[i][j]=0;
                     continue;
-                if(map[y-1][x]<0 && map[y][x-1]<0)
-                    map[y][x]=-1;
-                else if(map[y-1][x]<0)
-                    map[y][x]=map[y][x-1];
-                else if(map[y][x-1]<0)
-                    map[y][x]=map[y-1][x];
-                else
-                    map[y][x]=(map[y][x-1]+map[y-1][x])%1000000007;
+                }
+                map[i][j] = (map[i-1][j] + map[i][j-1])%1000000007;
             }
         }
-        return map[n][m];
+        return map[m][n];
     }
 }
