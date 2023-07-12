c876b6d4895b89921df7f789ce727653c8547c54
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-07-12 09:47:20 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 2.69 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,19 +1,8 @@
 class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
-        for(int i=0; i<numbers.length; i++){      
-            long n = numbers[i];
-            if((n&1)==0){
-                answer[i] = n + 1;
-                continue;
-            }
-                
-            long m = 1;
-            while((n & m) > 0)
-                m <<= 1;
-
-            answer[i] = m>n ? n+1+(n>>1) : (n|m)&(~(m>>1));
-        }
+        for(int i=0; i<numbers.length; i++)
+            answer[i] = numbers[i]+1 + ((numbers[i]^(numbers[i]+1))>>2);
         return answer;
     }
 }
