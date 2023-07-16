2e6ffbd90551c73bafadc7b371f7e3e222be6b9f
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-07-16 20:21:02 +0900

    [level 2] Title: 삼각 달팽이, Time: 6.32 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,20 +1,21 @@
 class Solution {
     public int[] solution(int n) {
         int[] answer = new int[n*(n+1)/2];
-        for(int step=0, idx=0, val=1; step<(n+1)/2; idx+=4*(++step)){
-            int width = n-3*step-1;
-            if(width==0){
+        for(int step=1, N=(n+2)/3, w=n-1, val=1, idx=0; step<=N; w-=3, idx+=4*step++){
+            if(w==0){
                 answer[idx]=val;
                 break;
             }
-            for(int i=0; i<3*width; i++){
-                answer[idx] = val++;
-                if(i/width==0)
-                    idx+=2*step+1+i;
-                else if(i/width==1)
-                    idx++;
-                else
-                    idx-=n-step-i%width;       
+            for(int i=0; i<w; i++){
+                answer[idx]=val++;
+                idx+=2*step-1+i;
+            }
+            for(int i=0; i<w; i++){
+                answer[idx++]=val++;
+            }
+            for(int i=0; i<w; i++){
+                answer[idx]=val++;
+                idx-=n+1-step-i;
             }
         }
         return answer;
