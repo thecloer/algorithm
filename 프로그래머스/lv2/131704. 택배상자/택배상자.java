0af69c95f7423ae7cc7723711600b52f3bc7244e
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-07-24 16:07:45 +0900

    [level 2] Title: 택배상자, Time: 157.69 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.java
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -3,19 +3,13 @@ class Solution {
     public int solution(int[] order) {
         Stack<Integer> sub = new Stack();
         int answer=0;
-        for(int i=1, p=0; i<=order.length && p<order.length; i++){
-            if(i == order[p]){
+        for(int i=1, p=0; i<=order.length; i++){
+            sub.push(i);
+            while(!sub.isEmpty() && sub.peek() == order[p]){
+                sub.pop();
                 answer++;
                 p++;
-                while(!sub.isEmpty()){
-                    if(sub.peek() != order[p])
-                        break;
-                    sub.pop();
-                    p++;
-                    answer++;
-                }
-            } else
-                sub.push(i);
+            }
         }
         
         return answer;
