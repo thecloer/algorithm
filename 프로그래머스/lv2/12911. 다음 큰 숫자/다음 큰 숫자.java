4faa1a4fd78588de756b619d5ece40b0fd100853
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-08-22 16:12:32 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,23 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int o = countOneBit(n);
-        
+        int o = Integer.bitCount(n);        
         int p=0;
         while((n&(3<<p)) != (1<<p))
             p++;
         p++;
         
         int ans = (n&(-1<<p)) + (1<<p);
-        ans += (1<<(o-countOneBit(ans)))-1;
+        ans += (1<<(o-Integer.bitCount(ans)))-1;
         return ans;
-    }
-    
-    int countOneBit(int n){
-        int o=0;
-        while(n>0){
-            n &= n-1;
-            o++;
-        }
-        return o;
     }
 }
