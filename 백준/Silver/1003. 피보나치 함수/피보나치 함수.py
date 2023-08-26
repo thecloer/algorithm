91520ba88f151653499589211e8721995a58c9ab
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-08-26 20:14:26 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,15 +1,11 @@
 import sys
 readline = sys.stdin.readline
+zeros, ones = [1, 0], [0, 1]
+for _ in range(41):
+  zeros.append(zeros[-2]+zeros[-1])
+  ones.append(ones[-2]+ones[-1])
+
 t = int(readline())
 inputs = [int(readline()) for _ in range(t)]
 for n in inputs:
-  if n == 0:
-    print('1 0')
-  elif n == 1:
-    print('0 1')
-  else:
-    zeros, ones = [1, 0], [0, 1]
-    for _ in range(n-1):
-      zeros.append(zeros[-2]+zeros[-1])
-      ones.append(ones[-2]+ones[-1])
-    print(f'{zeros[-1]} {ones[-1]}')
+  print(f'{zeros[n]} {ones[n]}')
