3f893e59cf28c696c70473830b4e5e5c5e136d31
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-08-29 02:57:13 +0900

    [Silver II] Title: 최대 힙, Time: 108 ms, Memory: 37180 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,11 +1,15 @@
 import sys, heapq
 readline = sys.stdin.readline
-N = int(readline())
-heap = []
-for _ in range(N):
-  n = int(readline())
-  if n:
-    heapq.heappush(heap, -n)
-  else:
-    if heap: print(-heapq.heappop(heap))
-    else: print(0)
+def main():
+  N = int(readline())
+  heap = []
+  for _ in range(N):
+    n = int(readline())
+    if n:
+      heapq.heappush(heap, -n)
+    else:
+      if heap: print(-heapq.heappop(heap))
+      else: print(0)
+
+if __name__ == "__main__":
+  main()
