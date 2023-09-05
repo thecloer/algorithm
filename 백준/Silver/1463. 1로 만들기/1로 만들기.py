1f3869576f5cf13ea963baca8c8755a5088f2cd5
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-05 19:31:43 +0900

    [Silver III] Title: 1로 만들기, Time: 56 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,14 +1,22 @@
 import sys
 
+def steps(dp, n):
+    if dp[n] == -1:
+        isEven = ((n & 1) == 0)
+        isMultipleOfThree = (n % 3 == 0)
+        if isEven and isMultipleOfThree:
+            dp[n] = 1 + min(steps(dp, n//3), steps(dp, n//2))
+        elif isEven:
+            dp[n] = 1 + min(steps(dp, n//2), steps(dp, n-1))
+        elif isMultipleOfThree:
+            dp[n] = 1 + min(steps(dp, n//3), steps(dp, n-1))
+        else:
+            dp[n] = 1 + steps(dp, n-1)
+    return dp[n]
+
 if __name__ == '__main__':
     read = sys.stdin.readline
     N = int(read())
-    dp = [0]*(N+1)
-    for i in range(N-1, 0, -1):
-        x = dp[i+1]
-        if i*3 <= N:
-            x = min(x, dp[i*3])
-        if i*2 <= N:
-            x = min(x, dp[i*2])
-        dp[i] = x + 1
-    print(dp[1])
+    dp = [-1] * (N+1)
+    dp[1] = 0
+    print(steps(dp, N))
