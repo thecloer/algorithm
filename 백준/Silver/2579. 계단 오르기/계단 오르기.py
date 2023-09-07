72fc0cb1e8fcd57ebf92ac68a62a74ae6e2fd27a
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-07 14:18:21 +0900

    [Silver III] Title: 계단 오르기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         scores = [0] * N
         scores[0] = steps[0]
         scores[1] = steps[0] + steps[1]
-        scores[2] = max(steps[0]+steps[2], steps[1]+steps[2])
+        scores[2] = max(steps[0], steps[1]) + steps[2]
         for i in range(3, N):
-            scores[i] = steps[i] + max(scores[i-2], scores[i-3] + steps[i-1])
+            scores[i] = max(scores[i-2], scores[i-3] + steps[i-1]) + steps[i]
         print(scores[N-1]) 
