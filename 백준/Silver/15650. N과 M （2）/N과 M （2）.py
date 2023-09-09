42beb0cb45de5e2ba4ada8760be0facbddacfb5e
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-09 17:24:34 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,22 +1,20 @@
-global N, M, pool
+def combinations(pool: tuple[int], r: int):
+    if r == 0 or len(pool) < r:
+        return []
+    if r == 1:
+        return [[x] for x in pool]
 
-def dfs(lst: list[int], poolStart: int):
-    global N, M, pool
-    depth = len(lst)
-    if depth == M:
-        print(*lst)
-        return
-    for i in range(poolStart, N):
-        if depth + N - i < M:
-            continue
-        lst.append(pool[i])
-        dfs(lst, i + 1)
-        lst.pop()
+    result = []
+    for i, x in enumerate(pool):
+        nextPool = pool[i+1:]
+        for c in combinations(nextPool, r - 1):
+            result.append([x] + c)
+    return result
 
 def main():
-    global N, M, pool
     N, M = map(int, input().split())
-    pool = tuple(range(1, N+1))
-    dfs([], 0)
+    result = combinations(tuple(range(1, N+1)), M)
+    for c in result:
+        print(*c)
 
 main()
