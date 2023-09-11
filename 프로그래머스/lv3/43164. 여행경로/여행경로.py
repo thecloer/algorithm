ef2386ed03f29ca85a9c9691d682be73b20a0d0d
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-11 12:01:36 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.py
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -3,17 +3,14 @@ def dfs(tickets, path, origin):
     if not tickets:
         return True
     
-    isDone = False
     for i, ticket in enumerate(tickets):
         if ticket[0] == origin:
             restTickets = tickets[:i] + tickets[i+1:]
             destination = ticket[1]
-            isDone = dfs(restTickets, path, destination)
-            if isDone:
-                break
-            else:
-                path.pop()
-    return isDone
+            if dfs(restTickets, path, destination):
+                return True
+            path.pop()
+    return False
 
 def solution(tickets):
     tickets.sort()
