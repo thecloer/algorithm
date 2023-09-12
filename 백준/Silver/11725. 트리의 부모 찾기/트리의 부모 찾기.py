a205710f45adbb4b8ce4b36153ff418185814d31
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-13 08:03:21 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 276 ms, Memory: 47700 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,24 +1,35 @@
-from collections import deque
 import sys
+sys.setrecursionlimit(100_000)
 read = sys.stdin.readline
 
 N = int(read())
-tree = [0] * (N + 1)
-tree[1] = 1
-graph = [set() for _ in range(N + 1)]
-for _ in range(N - 1):
-    a, b = map(int, read().split())
-    graph[a].add(b)
-    graph[b].add(a)
+parent = [0] * (N + 1)
+parent[1] = 1
 
-q = deque([1])
-while q:
-    root = q.popleft()
-    children = graph[root]
-    for child in children:
-        if tree[child] == 0:
-            tree[child] = root
-            q.append(child)
+
+def union(a, b):
+    if parent[a] == 0:
+        parent[a] = b
+        if parent[b] == 0:
+            parent[b] = a
+    elif parent[b] == 0:
+        parent[b] = a
+    elif parent[b] != a:
+        union(b, parent[b])
+        parent[b] = a
+
+
+ancestor = []
+for _ in range(N-1):
+    a, b = map(int, read().split())
+    if a == 1:
+        ancestor.append(b)
+    elif b == 1:
+        ancestor.append(a)
+    union(a, b)
+
+for anc in ancestor:
+    union(1, anc)
 
 for i in range(2, N + 1):
-    print(tree[i])
+    print(parent[i])
