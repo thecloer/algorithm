66640e4cd9c8cfddfeb8a6737bc4f9661f2872eb
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-14 12:07:52 +0900

    [Silver II] Title: N과 M (9), Time: 128 ms, Memory: 34324 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,4 +1,3 @@
-from collections import Counter
 import sys
 read = sys.stdin.readline
 
@@ -6,29 +5,26 @@ def permutations(pool, n, r):
     if r == 0 or r > n:
         return []
 
-    keys = list(pool.keys())
-    keys.sort()
+    keys = []
+    for x in pool:
+        if x not in keys:
+            keys.append(x)
+
     if r == 1:
         return [[x] for x in keys]
 
     result = []
     for x in keys:
-        pool[x] -= 1
-        if pool[x] == 0:
-            pool.pop(x)
-
-        for p in permutations(pool, n - 1, r - 1):
+        nextPool = pool[:]
+        nextPool.remove(x)
+        for p in permutations(nextPool, n - 1, r - 1):
             result.append([x] + p)
-
-        if x in pool:
-            pool[x] += 1
-        else:
-            pool[x] = 1
     return result
 
 def main():
     N, M = map(int, read().split())
-    pool = Counter(map(int, read().split()))
+    pool = list(map(int, read().split()))
+    pool.sort()
     for p in permutations(pool, N, M):
         print(*p)
 
