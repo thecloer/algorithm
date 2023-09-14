cf2b8bf68744f7363bc82c312e1435374700f88d
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-14 12:31:22 +0900

    [Silver III] Title: N과 M (3), Time: 3996 ms, Memory: 157008 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,17 +1,22 @@
-def product(pool: tuple[int], r: int):
+import sys
+read = sys.stdin.readline
+
+def product(pool: list[int], n: int, r: int):
     if r == 0:
         return []
     if r == 1:
         return [[x] for x in pool]
+
     result = []
     for x in pool:
-        for p in product(pool, r - 1):
+        for p in product(pool, n - 1, r - 1):
             result.append([x] + p)
     return result
 
 def main():
     N, M = map(int, input().split())
-    for p in product(tuple(range(1, N+1)), M):
+    pool = list(range(1, N+1))
+    for p in product(pool, N, M):
         print(*p)
 
 main()
