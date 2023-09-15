3c126494cebd9f291497c919c71a8e26d8913aec
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-16 01:28:36 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1420 ms, Memory: 43588 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+
+  int N, L, x;
+  deque<pair<int, int> > dq;
+
+  cin >> N >> L;
+  for(int i=0; i<N; i++){
+    cin >> x;
+    if(!dq.empty() && dq.front().first <= i - L)
+      dq.pop_front();
+    while(!dq.empty() && dq.back().second > x)
+      dq.pop_back();
+    dq.push_back(make_pair(i, x));
+    cout << dq.front().second << " ";
+  }
+  cout << endl;
+  return 0;
+}
