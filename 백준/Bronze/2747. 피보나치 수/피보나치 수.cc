582ff8a1f83ca1960eeb8d1f9ef63e5aba2bec2a
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-17 01:54:30 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -5,18 +5,13 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
     
-    int N, first, second;
-    first = 0; second = 1;
+    int N, first=0, second=1, tmp;
     cin >> N;
-    if(N==0)
-        cout << first;
-    else {
-        while(--N) {
-            int tmp = first + second;
-            first = second;
-            second = tmp;
-        }
-        cout << second;
+    while(--N) {
+        tmp = first;
+        first = second;
+        second += tmp;
     }
+    cout << second;
     return 0;
 }
