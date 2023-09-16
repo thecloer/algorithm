8dc3571595dfe3b852cfc0d9872cee3851f8aeac
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-17 01:26:52 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -3,23 +3,21 @@ using namespace std;
 
 inline int min(int x, int y) {return x < y ? x : y;}
 
-int N;
-int dp[1000][3];
+int n, R, G, B, r, g, b;
 
 int main(){
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
 
-	cin >> N;
-	for(int i=0; i<N; i++)
-		for(int j=0; j<3; j++)
-			cin >> dp[i][j];
-	
-	for(int i=1; i<N; i++){
-		dp[i][0] += min(dp[i-1][1], dp[i-1][2]);
-		dp[i][1] += min(dp[i-1][0], dp[i-1][2]);
-		dp[i][2] += min(dp[i-1][0], dp[i-1][1]);
-	}
-	cout << min(dp[N-1][0], min(dp[N-1][1], dp[N-1][2]));
-	return 0;
+    cin >> n;
+    while(n--){
+        cin >> r >> g >> b;
+        r += min(G, B);
+        g += min(R, B);
+        b += min(R, G);
+        
+        R = r; G = g; B = b;
+    }
+    cout << min(min(R, G), B);
+    return 0;
 }
