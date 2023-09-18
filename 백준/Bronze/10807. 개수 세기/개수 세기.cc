e1c61e9cb4885ec21a29a5e4d0622cf70a7d0c62
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-18 16:15:00 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cc
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);cout.tie(0);
   
-  int N, v, ans=0;
-  int a[100];
+  int N, v;
+  int a[201]={};
   cin >> N;
-  for(int i=0; i<N; i++)
-    cin >> a[i];
-  cin >>v;
-  for(int i=0; i<N; i++)
-    if(a[i]==v) ans++;
-  cout<<ans;
+  for(int i=0; i<N; i++){
+    int t;
+    cin >> t;
+    a[t+100]++;
+  }
+  cin >> v;
+  cout << a[v+100];
 }
