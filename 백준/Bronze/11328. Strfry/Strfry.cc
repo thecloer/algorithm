5e598442cf7f84120500c4e447cb23e4244d0d74
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-18 16:57:03 +0900

    [Bronze II] Title: Strfry, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.cc
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -9,27 +9,20 @@ int main(){
   int N;
   cin >> N;
   while(N--){
-    string a,b;
-    cin >> a >> b;
-    int aa[26] = {};
-    int bb[26] = {};
-    if(a.length() == b.length()){
-      for(int i=0; i<a.length(); i++){
-        aa[a[i]-'a']++; 
-        bb[b[i]-'a']++;
+    int a[26] = {};
+    string s1,s2;
+    cin >> s1 >> s2;
+
+    for(auto c:s1) a[c-'a']++;
+    for(auto c:s2) a[c-'a']--;
+    
+    bool isPossible = true;
+    for(int i:a){
+      if(i != 0){
+        isPossible = false;
+        break;
       }
-      bool isPossible = true;
-      for(int i=0; i<26; i++){
-        if(aa[i] != bb[i]) {
-          isPossible = false;
-          break;
-        }
-      }
-      if(isPossible){
-        cout << "Possible\n";  
-        continue;
-      }
-    } 
-    cout << "Impossible\n";
+    }
+    cout << (isPossible ? "Possible\n" : "Impossible\n");
   }
 }
