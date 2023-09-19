e34d65fc78c4638e1cff5c7e187fce630dc4d38f
Seokgyu Choi <83699438+thecloer@users.noreply.github.com>
2023-09-19 12:59:33 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 40 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,26 +1,31 @@
 #include <iostream>
-#include <list>
 using namespace std;
+
+const int MAX = 5001;
+int nxt[MAX];
+int pre[MAX];
 
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);cout.tie(0);
-  int N,K;
+
+  int N, K, len;
   cin >> N >> K;
 
-  list<int> L;
-  for(int i=1; i<=N; i++) L.push_back(i);
+  len = N;
+  for(int i=1; i<=N; i++) {
+    nxt[i] = (i==N) ? 1 : i+1;
+    pre[i] = (i==1) ? N : i-1;
+  }
 
   cout << '<';
-  auto p = L.begin();
-  int i = 1;
-  while(!L.empty()){
-    if(i%K==0) {
-      cout << *p;
-      p = L.erase(p);
-      cout << (L.empty() ? ">" : ", ");
-    } else p++;
-    if(p == L.end()) p = L.begin();
-    i++;
+  for(int i = 1, cur = 1; len > 0; cur = nxt[cur]){
+    if(i == K) {
+      i = 1;
+      len--;
+      nxt[pre[cur]] = nxt[cur];
+      pre[nxt[cur]] = pre[cur];
+      cout << cur << (len == 0 ? ">" : ", ");
+    } else ++i;
   }
 }
