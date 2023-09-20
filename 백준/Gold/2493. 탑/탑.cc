dbc5690b6613303d2c1fddd76a63fa0601996903
thecloer <83699438+thecloer@users.noreply.github.com>
2023-09-21 00:42:02 +0900

    [Gold V] Title: 탑, Time: 104 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,28 +1,24 @@
 #include <iostream>
 #include <stack>
 using namespace std;
+#define idx first
+#define height second
 
-struct building {
-  int idx, height;
-};
+int N;
+stack<pair<int,int>> S;
 
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);cout.tie(0);
 
-  int N;
   cin >> N;
-  stack<building> S;
-  for(int i=0, x; i<N; i++){
-    cin >> x;
-    while(!S.empty() && S.top().height <= x) S.pop();
-
-    if(S.empty()) cout << "0 ";
-    else cout << S.top().idx << ' ';
-    
-    building b;
-    b.idx = i+1;
-    b.height = x;
-    S.push(b);
+  S.push({0, 100000001});
+  for (int i = 1; i <= N; i++) {
+    int height;
+    cin >> height;
+    while (S.top().height < height)
+      S.pop();    
+    cout << S.top().idx << " ";
+    S.push({i, height});
   }
 }
