cac4c67bcba7205cafa9c3012843b67842cd5bab
thecloer <83699438+thecloer@users.noreply.github.com>
2023-09-22 17:11:07 +0900

    [Platinum V] Title: 히스토그램, Time: 8 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.cc
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -8,25 +8,22 @@ int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);cout.tie(0);
   
-  stack<pair<int, int>> s;
   long long ans = 0;
   int n;
   cin >> n;
-  for(int i=0; i<n; i++){
-    int x, idx=i;
-    cin >> x;
-    while(!s.empty() && s.top().height > x){
-      auto t = s.top(); 
-      s.pop();
-      ans = max(ans, 1LL * t.height * (i - t.idx));
-      idx = t.idx;
+  stack<pair<int,int>> S;
+  for(int i=0; i<=n; i++){
+    int x = 0, idx = i;
+    if(x != n) cin >> x;
+    while(!S.empty() && S.top().height > x) {
+      int h = S.top().height;
+      int w = i - S.top().idx;
+      idx = S.top().idx;
+      ans = max(ans, 1LL * h * w);
+      S.pop();
     }
-    if(s.empty() || x != s.top().height) 
-      s.push({idx, x});
-  }
-  while(!s.empty()){
-      ans = max(ans, 1LL * s.top().height * (n - s.top().idx));
-      s.pop();
+    if(S.empty() || S.top().height != x)
+      S.push({idx, x});
   }
   cout << ans;
 }
