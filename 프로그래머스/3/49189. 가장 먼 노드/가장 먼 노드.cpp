25305b6268531c5731726c1f27353349ab9feda7
thecloer <83699438+thecloer@users.noreply.github.com>
2023-10-11 02:21:15 +0900

    [level 3] Title: 가장 먼 노드, Time: 12.06 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -1,27 +1,28 @@
 #include <vector>
+#include <unordered_map>
 #include <queue>
 using namespace std;
-bool matrix[20001][20001];
-bool vis[20001];
+
+bool used[20001];
 
 int solution(int n, vector<vector<int>> edge) {
+    unordered_map<int, vector<int>> adj;
     for(auto &e:edge) {
-        matrix[e[0]][e[1]] = 1;
-        matrix[e[1]][e[0]] = 1;
+        adj[e[0]].push_back(e[1]);
+        adj[e[1]].push_back(e[0]);
     }
     queue<int> Q;
+    used[1] = 1;
     Q.push(1);
-    vis[1] =1;
     int size;
     while(!Q.empty()) {
         size = Q.size();
         for(int i=0; i<size; i++) {
             int x = Q.front(); Q.pop();
-            for(int nx=1; nx<=n; nx++) {
-                if(matrix[x][nx] && !vis[nx]) {
-                    vis[nx] = 1;
-                    Q.push(nx);
-                }
+            for(int nx:adj[x]) {
+                if(used[nx]) continue;
+                used[nx] = 1;
+                Q.push(nx);
             }
         }
     }
