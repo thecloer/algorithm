16de9da19623280afe1231108f5ddaa91ad9f7d7
thecloer <83699438+thecloer@users.noreply.github.com>
2023-10-11 02:02:15 +0900

    [level 3] Title: 가장 먼 노드, Time: 12.62 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+bool used[20001];
+
+int solution(int n, vector<vector<int>> edge) {
+    unordered_map<int, vector<int>> adj;
+    for(auto &e:edge) {
+        adj[e[0]].push_back(e[1]);
+        adj[e[1]].push_back(e[0]);
+    }
+    queue<int> Q;
+    Q.push(1);
+    used[1] = 1;
+    int size;
+    while(!Q.empty()) {
+        size = Q.size();
+        for(int i=0; i<size; i++) {
+            int x = Q.front(); Q.pop();
+            for(int nx : adj[x]) {
+                if(used[nx]) continue;
+                used[nx] = 1;
+                Q.push(nx);
+            }
+        }
+    }
+    return size;
+}
