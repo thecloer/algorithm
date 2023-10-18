97638d6d8ffcfcd29aef4d6d439abd23c158e79b
thecloer <83699438+thecloer@users.noreply.github.com>
2023-10-19 05:35:21 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 484 ms, Memory: 47816 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -9,41 +9,29 @@ int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int N, M, K;
-    cin >> N >> M >> K;
+    ios::sync_with_stdio(0); cin.tie(0);
+    int N, M, K; cin >> N >> M >> K;
+    for(int i=0; i<N; i++) cin >> board[i];
     int endX = N-1, endY = M-1;
-    if(!(endX || endY)) {
-        cout << 1;
-        return 0;
-    }
-    for(int i=0; i<N; i++) 
-        cin >> board[i];
-    int ans = -1;
+
     queue<xyk> Q;
     dist[0][0][0] = 1;
     Q.push({0, 0, 0});
-    while(!(Q.empty() || ans > 0)) {
+    while(!Q.empty()) {
         xyk cur = Q.front(); Q.pop();
+        if(cur.x == endX && cur.y == endY) {
+            cout << dist[cur.x][cur.y][cur.k];
+            return 0;
+        }
         for(int d=4; d--;) {
             int nx = cur.x + dx[d];
             int ny = cur.y + dy[d];
             if(nx<0 || nx>endX || ny<0 || ny>endY) continue;
-            if(nx == endX && ny == endY) {
-                ans = dist[cur.x][cur.y][cur.k] + 1;
-                break;
-            }
-            int nk = cur.k;
-            if(board[nx][ny] == '1') {
-                if(nk < K) nk++;
-                else continue;
-            }
-            if(dist[nx][ny][nk]) continue;
+            int nk = board[nx][ny] == '1' ? cur.k + 1 : cur.k;
+            if(nk > K || dist[nx][ny][nk]) continue;
             dist[nx][ny][nk] = dist[cur.x][cur.y][cur.k] + 1;
             Q.push({nx, ny, nk});
         }
     }
-    if(ans > 0) cout << ans;
-    else cout << -1;
+    cout << -1;
 }
