a15bcbfaee5472cadaa7014aa3a0b3173bee51e7
thecloer <83699438+thecloer@users.noreply.github.com>
2023-10-19 08:09:52 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 460 ms, Memory: 15736 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cc
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cc
@@ -2,9 +2,9 @@
 #include <queue>
 using namespace std;
 
-struct xykd {int x, y, k, dist;};
+struct xyk {int x, y, k;};
 char board[1000][1001];
-int dist[1000][1000][11];
+bool vis[1000][1000][11];
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
 
@@ -14,31 +14,33 @@ int main() {
     int endX = N-1, endY = M-1;
     for(int i=0; i<N; i++) cin >> board[i];
 
-    queue<xykd> Q;
-    dist[0][0][0] = 1;
-    Q.push({0, 0, 0, 1});
+    queue<xyk> Q;
+    Q.push({0, 0, 0});
+    vis[0][0][0] = true;
+    int ans = 1;
     while(!Q.empty()) {
-        auto [x, y, k, d] = Q.front(); Q.pop();
-        if(x == endX and y == endY) {
-            cout << dist[x][y][k];
-            return 0;
-        }
-        int nd = d + 1;
-        bool wait = false;
-        for(int i=4; i--;) {
-            int nx = x + dx[i], ny = y + dy[i];
-            if(nx<0 or nx>endX or ny<0 or ny>endY) continue;
-            int nk = board[nx][ny] == '1' ? k + 1 : k;
-            if(nk > K) continue;
-            if(dist[nx][ny][nk]) continue;
-            if(board[nx][ny] == '1' and !(d & 1)) wait = true;
-            else {
-                dist[nx][ny][nk] = nd;
-                Q.push({nx, ny, nk, nd});
+        int size = Q.size();
+        while(size--) {
+            auto [x, y, k] = Q.front(); Q.pop();
+            if(x == endX and y == endY) {
+                cout << ans;
+                return 0;
             }
+            bool wait = false;
+            for(int i=4; i--;) {
+                int nx = x + dx[i], ny = y + dy[i];
+                if(nx<0 or nx>endX or ny<0 or ny>endY) continue;
+                int nk = board[nx][ny] == '1' ? k + 1 : k;
+                if(nk > K or vis[nx][ny][nk]) continue;
+                if(board[nx][ny] == '1' and !(ans & 1)) wait = true;
+                else {
+                    vis[nx][ny][nk] = true;
+                    Q.push({nx, ny, nk});
+                }
+            }
+            if(wait) Q.push({x, y, k});
         }
-        if(wait) Q.push({x, y, k, nd});
+        ans++;
     }
-    
     cout << -1;
 }
