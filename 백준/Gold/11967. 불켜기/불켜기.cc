cd214242908180fedcc9a85ab4adb279de474d43
thecloer <83699438+thecloer@users.noreply.github.com>
2023-10-21 21:32:15 +0900

    [Gold II] Title: 불켜기, Time: 8 ms, Memory: 2812 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.cc
+++ 백준/Gold/11967. 불켜기/불켜기.cc
@@ -10,6 +10,17 @@ bool vis[101][101];
 vector<xy> btns[101][101];
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
+queue<xy> Q;
+
+bool OoB(int x, int y) {return x<1 || x>N || y<1 || y>N;};
+bool isConnected(xy &pos) {
+    for(int i=4; i--;) {
+        int nx = pos.x + dx[i], ny = pos.y + dy[i];
+        if(OoB(nx, ny)) continue;
+        if(vis[nx][ny]) return true;
+    }
+    return false;
+}
 
 int main() {
     ios::sync_with_stdio(0);
@@ -20,7 +31,7 @@ int main() {
         cin >> x >> y >> a >> b;
         btns[x][y].push_back({a, b});
     }
-    queue<xy> Q;
+
     int cnt = 1;
     lights[1][1] = true;
     vis[1][1] = true;
@@ -29,22 +40,15 @@ int main() {
         xy cur = Q.front(); Q.pop();
         auto &curBtns = btns[cur.x][cur.y];
         while(!curBtns.empty()) {
-            const xy &btn = curBtns.back(); curBtns.pop_back();
-            if(btn.x && btn.y && !lights[btn.x][btn.y]) {
-                lights[btn.x][btn.y] = true;
-                cnt++;
-                for(int i=4; i--;) {
-                    int nx = btn.x + dx[i], ny = btn.y + dy[i];
-                    if(nx<1 || nx>N || ny<1 || ny>N) continue;
-                    if(vis[nx][ny]) {
-                        vis[btn.x][btn.y] = true;
-                        Q.push(btn);
-                        break;
-                    }
-                }
+            xy &btn = curBtns.back(); curBtns.pop_back();
+            if(lights[btn.x][btn.y]) continue;
+            lights[btn.x][btn.y] = true;
+            cnt++;
+            if(isConnected(btn)) {
+                Q.push(btn);
+                vis[btn.x][btn.y] = true;
             }
         }
-
         for(int i=4; i--;) {
             int nx = cur.x + dx[i], ny = cur.y + dy[i];
             if(nx<1 || nx>N || ny<1 || ny>N) continue;
