a81a7d10d1f4964dc6d0bc0e357f91d0f4404499
thecloer <83699438+thecloer@users.noreply.github.com>
2023-10-29 04:44:58 +0900

    [Gold IV] Title: N-Queen, Time: 1696 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,39 +1,35 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 struct xy { int x, y; };
 int n, ans;
-vector<xy> queens;
-bool cols[15];
+bool cols[14];
+bool diagonals[2][29];
 
-bool checkDiagonal(int r, int c) {
-    for(xy p:queens) 
-        if(abs(r - p.x) == abs(c - p.y)) 
+bool checkDiagonals(int r, int c) {
+    for(int i=0; i<n; i++)
+        if(diagonals[i][0] || diagonals[i][1])
             return true;
     return false;
 }
-
 void backTracking(int r) {
     if(r == n) {
         ans++;
         return;
     }
     for(int c=0; c<n; c++) {
-        if(cols[c]) continue;
-        if(checkDiagonal(r, c)) continue;
+        if(cols[c] || diagonals[0][r+c] || diagonals[1][r-c+n-1]) continue;
         cols[c] = true;
-        queens.push_back({r, c});
+        diagonals[0][r+c] = true;
+        diagonals[1][r-c+n-1] = true;
         backTracking(r+1);
-        queens.pop_back();
+        diagonals[0][r+c] = false;
+        diagonals[1][r-c+n-1] = false;
         cols[c] = false;
     }
 }
-
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
+    ios::sync_with_stdio(0); cin.tie(0);
     cin >> n;
     backTracking(0);
     cout << ans;
