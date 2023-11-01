efd570c73e78f3c117ab78e6432100425454c3bd
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-01 12:31:34 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.cc
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -3,32 +3,28 @@
 using namespace std;
 
 int L, C;
-char input[15];
-char output[15];
+char input[15], output[15];
 
 bool isVowel(char c) {
     return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
 }
 void backTracking(int cur, int len, int vowelCnt) {
     if(len == L) {
-    	if(vowelCnt < 1 || len-vowelCnt < 2)
-    		return;
-        for(int i=0; i<L; i++) 
-        	cout << output[i];
-		 cout << '\n';
-		 return;
+        if(vowelCnt < 1 || len-vowelCnt < 2) return;
+        for(int i=0; i<L; i++) cout << output[i];
+        cout << '\n';
+        return;
     }
     for(int i=cur; i<C; i++) {
         output[len] = input[i];
-        backTracking(i+1, len+1, vowelCnt+isVowel(input[i]));
+        backTracking(i+1, len+1, vowelCnt + isVowel(input[i]));
     }
 }
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> L >> C;
     for(int i=0; i<C; i++) cin >> input[i];
+
     sort(input, input+C);
     backTracking(0, 0, 0);
-	return 0;
 }
