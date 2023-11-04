7818731dad9bf64f91b998624d5c3fd1ebcd3ba6
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-04 09:09:25 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,35 +1,37 @@
-select c.car_id, c.car_type, 
-    floor(30 * c.daily_fee * (1-p.discount_rate/100)) as fee
+# select c.car_id, c.car_type, 
+#     floor(30 * c.daily_fee * (1-p.discount_rate/100)) as fee
+# from car_rental_company_car as c
+# join car_rental_company_discount_plan as p
+#     on c.car_type = p.car_type
+# # join car_rental_company_rental_history as h
+# #     on c.car_id = h.car_id
+# where c.car_type in ('SUV', '세단')
+#     and duration_type = '30일 이상'
+#     # and (start_date >= '2022-12-01' or end_date < '2022-11-01')
+#     and 30 * daily_fee * (1-discount_rate/100) >= 500000
+#     and 30 * daily_fee * (1-discount_rate/100) < 2000000
+#     and c.car_id not in (
+#         select car_id
+#         from car_rental_company_rental_history
+#         where end_date >= '2022-11-01' and start_date < '2022-12-01'
+#     )
+# order by fee desc, c.car_type, c.car_id desc;
+
+select car_id, c.car_type, 
+    floor(30 * daily_fee * (1-discount_rate/100)) as fee
 from car_rental_company_car as c
-join car_rental_company_discount_plan as p
-    on c.car_type = p.car_type
-# join car_rental_company_rental_history as h
-#     on c.car_id = h.car_id
-where c.car_type in ('SUV', '세단')
-    and duration_type = '30일 이상'
-    # and start_date >= '2022-12-01' or end_date < '2022-11-01'
-    and 30 * daily_fee * (1-discount_rate/100) >= 500000
-    and 30 * daily_fee * (1-discount_rate/100) < 2000000
-    and c.car_id not in (
+join (
+    select car_type, discount_rate
+    from car_rental_company_discount_plan
+    where car_type in ('SUV', '세단') and duration_type = '30일 이상'
+) p on c.car_type = p.car_type
+where 
+    car_id not in (
         select car_id
         from car_rental_company_rental_history
-        where end_date >= '2022-11-01' and start_date < '2022-12-01'
+        # where start_date >= '2022-12-01' or end_date < '2022-11-01'
+        where start_date < '2022-12-01' and end_date >= '2022-11-01'
     )
+    and 30 * daily_fee * (1-discount_rate/100) >= 500000 
+    and 30 * daily_fee * (1-discount_rate/100) < 2000000
 order by fee desc, c.car_type, c.car_id desc;
-
-# select c.car_id, c.car_type, 
-#     floor(30 * daily_fee * (1-discount_rate/100)) as fee
-# from car_rental_company_car as c
-# join (
-#     select car_type, discount_rate
-#     from car_rental_company_discount_plan
-#     where car_type in ('SUV', '세단') and duration_type = '30일 이상'
-# ) p on c.car_type = p.car_type
-# join (
-#     select car_id
-#     from car_rental_company_rental_history
-#     where start_date > '2022-11-30' or end_date < '2022-11-01'
-# ) h on c.car_id = h.car_id
-# where 30 * daily_fee * (1-discount_rate/100) >= 500000 
-#     and 30 * daily_fee * (1-discount_rate/100) < 2000000
-# order by fee desc, c.car_type, c.car_id desc;
