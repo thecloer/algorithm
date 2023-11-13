02ae3a3e46853b292c840966378f00bdbd36a507
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-14 06:07:15 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 36 ms, Memory: 2280 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+#define ll long long
+using namespace std;
+
+struct rect {int idx, h;};
+ll max(ll a, ll b) {return a > b ? a : b;}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    while(true) {
+        int n; cin >> n;
+        if(n == 0) break;
+        stack<rect> S;
+        ll ans = 0;
+        for(int i=0; i<=n; i++){
+            int x = 0, idx = i;
+            if(i != n) cin >> x;
+            while(!S.empty() && S.top().h > x) {
+                idx = S.top().idx;
+                ans = max(ans, (ll)S.top().h * (i - idx));
+                S.pop();
+            }
+            if(S.empty() || S.top().h != x)
+                S.push({idx, x});
+        }
+        cout << ans << '\n';
+    }
+}
