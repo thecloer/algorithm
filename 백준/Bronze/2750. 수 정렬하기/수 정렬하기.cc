d576d10bfb74902a16784199c07ace79a955f6c5
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-14 07:56:32 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,31 +1,12 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-
-void merge(int arr[], int start, int end) {
-    int tmp[end-start];
-    int mid = (start + end) / 2;
-    int left = start, right = mid;
-    for(int i = 0; i < end-start; i++) {
-        if(right == end) tmp[i] = arr[left++];
-        else if(left == mid) tmp[i] = arr[right++];
-        else if(arr[left] <= arr[right]) tmp[i] = arr[left++];
-        else tmp[i] = arr[right++];
-    }
-    for(int i=start; i<end; i++) arr[i] = tmp[i-start];
-}
-void mergeSort(int arr[], int start, int end) {
-    if(end - start <= 1) return;
-    int mid = (start + end) / 2;
-    mergeSort(arr, start, mid);
-    mergeSort(arr, mid, end);
-    merge(arr, start, end);
-}
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int n; cin >> n;
     int arr[n];
     for(int &x:arr) cin >> x;
-    mergeSort(arr, 0, n);
+    sort(arr, arr + n);
     for(const int &x:arr) cout << x << '\n';
 }
