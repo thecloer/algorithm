e79a461a2665c61112e173872c50c3812ee0ca2c
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-14 04:33:46 +0900

    [Gold IV] Title: 뱀, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.cc
+++ 백준/Gold/3190. 뱀/뱀.cc
@@ -1,48 +1,34 @@
 #include <iostream>
-#include <queue>
 using namespace std;
 
 struct command {char cmd; int time;};
-int N, K, sec, dir, len = 1, x = 1, y = 1;
 int board[101][101];
 int dx[] = {0, 1, 0, -1};
 int dy[] = {1, 0, -1, 0};
-queue<command> cmds;
 
-void move() {
-    x += dx[dir]; y += dy[dir];
-    if(board[x][y] == -2) len++;
-}
-bool gameOver() {
-    return x<1 || x>N || y<1 || y>N || board[x][y] >= sec - len;
-}
-void turn(char cmd) {
-    dir = (cmd == 'D' ? ++dir : --dir + 4 ) % 4;
-}
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    cin >> N;
+    int N, K; cin >> N >> K;
     for(int i=1; i<=N; i++)
-        for(int j=1; j<=N; j++)
+        for(int j=1; j<=N; j++) 
             board[i][j] = -1;
-    cin >> K;
     while(K--) {
         int a, b; cin >> a >> b;
         board[a][b] = -2;
     }
-    cin >> K;
-    while(K--) {
-        int X; char C; cin >> X >> C;
-        cmds.push({C, X});
-    }
+    int L; cin >> L;
+    command cmds[L];
+    for(int i=0; i<L; i++) 
+        cin >> cmds[i].time >> cmds[i].cmd;
+    
+    int sec = 0, dir = 0, len = 1, x = 1, y = 1, cmdIdx = 0;
     while(true) {
         board[x][y] = sec++;
-        move();
-        if(gameOver()) break;
-        if(!cmds.empty() && sec == cmds.front().time) {
-            turn(cmds.front().cmd);
-            cmds.pop();
-        }
+        x += dx[dir]; y += dy[dir];
+        if(board[x][y] == -2) len++;
+        if(x<1 || x>N || y<1 || y>N || board[x][y] >= sec - len) break;
+        if(cmdIdx < L && sec == cmds[cmdIdx].time)
+            dir = (cmds[cmdIdx++].cmd == 'D' ? ++dir : --dir + 4 ) % 4;
     }
     cout << sec;
 }
