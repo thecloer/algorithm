0f5e78ef617ec1feb5012bc30cdd4193850da6df
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-15 14:18:05 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.cc
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.cc
@@ -1,82 +1,80 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-#define X first
-#define Y second
+struct loc {int rx, ry, bx, by;};
+int N, M;
+char board[10][10];
+bool vis[10][10][10][10];
+int dx[] = {1, 0, -1, 0};
+int dy[] = {0, 1, 0, -1};
 
-int n, m;
-pair<int,int> red, blue;
-string board[11];
-int dist[11][11][11][11]; 
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {1, 0, -1, 0};
-
-int bfs() {
-  queue<tuple<int, int, int, int>> q;
-  q.push({red.X, red.Y, blue.X, blue.Y});
-  dist[red.X][red.Y][blue.X][blue.Y] = 0;
-  while (!q.empty()) {
-    int rx, ry, bx, by;
-    tie(rx, ry, bx, by) = q.front();
-    q.pop();
-    int cnt = dist[rx][ry][bx][by];
-    if (cnt >= 10)
-      return -1;
-    for (int i = 0; i < 4; i++) {
-      int n_rx = rx, n_ry = ry, n_bx = bx, n_by = by;
-
-      while (board[n_bx + dx[i]][n_by + dy[i]] == '.'){
-        n_bx += dx[i];
-        n_by += dy[i];
-      }
-      if(board[n_bx + dx[i]][n_by + dy[i]] == 'O') continue;
-
-      while (board[n_rx + dx[i]][n_ry + dy[i]] == '.'){
-        n_rx += dx[i];
-        n_ry += dy[i];
-      }
-      if(board[n_rx + dx[i]][n_ry + dy[i]] == 'O') return cnt+1;
-
-      if ((n_rx == n_bx) && (n_ry == n_by)) {
-        if (i == 0) // 위쪽
-          ry < by ? n_ry-- : n_by--;
-        else if (i == 1) // 오른쪽
-          rx < bx ? n_rx-- : n_bx--;
-        else if (i == 2) // 아래쪽
-          ry > by ? n_ry++ : n_by++;
-        else // 왼쪽
-          rx > bx ? n_rx++ : n_bx++;
-      }
-      if (dist[n_rx][n_ry][n_bx][n_by] != -1) continue;
-      dist[n_rx][n_ry][n_bx][n_by] = cnt + 1;
-      q.push({n_rx, n_ry, n_bx, n_by});
-    }
-  }
-  return -1;
+char nextMove(int x, int y, int dir) {
+    return board[x + dx[dir]][y + dy[dir]];
 }
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+void move(int &x, int &y, int dir) {
+    x += dx[dir]; y += dy[dir];
+}
 
-  for(int i = 0; i < 10; i++)
-    for(int j = 0; j < 10; j++)
-      for(int k = 0; k < 10; k++)
-        fill(dist[i][j][k], dist[i][j][k]+10, -1);
-  
-  cin >> n >> m;
-  for (int i = 0; i < n; i++) {
-    cin >> board[i];
-    for (int j = 0; j < m; j++) {      
-      if (board[i][j] == 'B'){
-        blue = {i, j};
-        board[i][j] = '.';
-      }
-      else if (board[i][j] == 'R'){
-        red = {i, j};
-        board[i][j] = '.';
-      }
+int bfs(const loc &start) {
+    int cnt = 0;
+    queue<loc> Q;
+    Q.push(start);
+    vis[start.rx][start.ry][start.bx][start.by] = true;
+    while(!Q.empty()) {
+        if(++cnt > 10) return -1;
+        for(int size = Q.size(); size--;) {
+            auto [rx, ry, bx, by] = Q.front(); Q.pop();
+            for(int dir=4; dir--;) {
+                // 파란공 이동
+                int nbx = bx, nby = by;
+                while(nextMove(nbx, nby, dir) == '.') move(nbx, nby, dir);
+                if(nextMove(nbx, nby, dir) == 'O') continue;
+                
+                // 빨간공 이동
+                int nrx = rx, nry = ry;
+                while(nextMove(nrx, nry, dir) == '.') move(nrx, nry, dir);
+                if(nextMove(nrx, nry, dir) == 'O') return cnt;
+
+                // 두 공이 겹친 경우 늦게 출발한 공 한칸 뒤로 이동
+                if((nrx == nbx) and (nry == nby)) {
+                    if(dir == 0) // 남
+                        rx < bx ? nrx-- : nbx--;
+                    else if(dir == 1) // 동
+                        ry < by ? nry-- : nby--;
+                    else if(dir == 2) // 북
+                        rx > bx ? nrx++ : nbx++;
+                    else // 서
+                        ry > by ? nry++ : nby++;
+                }
+
+                if(vis[nrx][nry][nbx][nby]) continue;
+                vis[nrx][nry][nbx][nby] = true;
+                Q.push({nrx, nry, nbx, nby});
+            }
+        }
     }
-  }
-  
-  cout << bfs();
+    return -1;
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    loc start;
+    cin >> N >> M;
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<M; j++) {
+            cin >> board[i][j];
+            if(board[i][j] == 'R') {
+                board[i][j] = '.';
+                start.rx = i;
+                start.ry = j;
+            }
+            else if(board[i][j] == 'B') {
+                board[i][j] = '.';
+                start.bx = i;
+                start.by = j;
+            }
+        }
+    };
+    cout << bfs(start);
 }
