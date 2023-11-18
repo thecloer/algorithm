9282c211c766762ef366bc1d8edb57193ffa6e36
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-19 00:51:47 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int dp[11];
-
-int fibo(int x) {
-    if(dp[x]) return dp[x];
-    return fibo(x-1) + fibo(x-2) + fibo(x-3);
-}
-
+int dp[12] = {1, 1, 2};
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int T; cin >> T;
-    dp[0] = dp[1] = 1; dp[2] = 2;
+    int i=3;
     while(T--) {
         int n; cin >> n;
-        cout << fibo(n) << '\n';
+        if(!dp[n]) {
+            while(i <= n) {
+                dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+                i++;
+            }
+        }
+        cout << dp[n] << '\n';
     }
 }
