df1704baaf790d8ef4604f41093bb07db0895a96
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-20 01:53:02 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,22 +1,16 @@
 #include <iostream>
 using namespace std;
 
-struct product {int w, v;};
-int maxValue[101][100001];
-product products[101];
+int dp[100001];
+inline int max(int a, int b) {return a > b ? a : b;}
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    int N, K; cin >> N >> K;
-    for(int i=1; i<=N; i++) 
-        cin >> products[i].w >> products[i].v;
-    for(int i=1; i<=N; i++) {
-        for(int j=1; j<=K; j++) {
-            maxValue[i][j] = maxValue[i-1][j];
-            if(j >= products[i].w)
-                maxValue[i][j] = max(maxValue[i][j], 
-                                    products[i].v + maxValue[i-1][j - products[i].w]);
-        }
+    int N, K, w, v; cin >> N >> K;
+    while(N--) {
+        cin >> w >> v;
+        for(int i = K; i >= w; i--)
+            dp[i] = max(dp[i], v + dp[i - w]);
     }
-    cout << maxValue[N][K];
+    cout << dp[K];
 }
