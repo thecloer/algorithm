9688ab4eb959d7fe6d00d18bdb0e150d0ea87c49
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-22 13:19:27 +0900

    [Gold I] Title: 구간 합 구하기, Time: 212 ms, Memory: 18404 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -2,20 +2,25 @@
 #define ll long long
 using namespace std;
 
-ll N, M, K, leafSize = 1;
+int N, M, K, leafSize = 1;
 ll segment[(1 << 21)];
 
 inline int left(int node) {return 2*node;}
 inline int right(int node) {return 2*node+1;}
 
-ll sum(ll L, ll R, ll node, ll nodeL, ll nodeR) {
+void init() {
+    for(int i = leafSize; i < leafSize+N; i++) 
+        cin >> segment[i];
+    for(int i = leafSize-1; i > 0; i--)
+        segment[i] = segment[left(i)] + segment[right(i)];
+}
+ll sum(int L, int R, int node, int nodeL, int nodeR) {
     if(R < nodeL || L > nodeR) return 0; 
     if(L <= nodeL and nodeR <= R) return segment[node];
-    ll mid = (nodeL + nodeR) / 2;
+    int mid = (nodeL + nodeR) / 2;
     return sum(L, R, left(node), nodeL, mid) + sum(L, R, right(node), mid+1, nodeR);
 }
-
-void update(ll i, ll val) {
+void update(int i, ll val) {
     i = leafSize + i - 1;
     segment[i] = val;
     while(i >>= 1)
@@ -27,15 +32,18 @@ int main() {
     cin >> N >> M >> K;
     while(leafSize < N) leafSize <<= 1;
 
-    for(int i = leafSize; i < leafSize+N; i++) 
-        cin >> segment[i];
-    for(int i = leafSize-1; i > 0; i--)
-        segment[i] = segment[left(i)] + segment[right(i)];
+    init();
     
-    ll Q = M+K;
+    int Q = M+K;
     while(Q--) {
-        ll a, b, c; cin >> a >> b >> c;
-        if(a == 1) update(b, c);
-        else cout << sum(b, c, 1, 1, leafSize) << '\n';
+        int queryType; cin >> queryType;
+        if(queryType == 1) {
+            int i; ll val; cin >> i >> val;
+            update(i, val);
+        }
+        else {
+            int L, R; cin >> L >> R;
+            cout << sum(L, R, 1, 1, leafSize) << '\n';
+        }
     }
 }
