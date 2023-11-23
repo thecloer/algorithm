fb25bd7cb1245dcf3787a00c7b8610b019275671
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-23 18:33:35 +0900

    [Silver V] Title: 단어 정렬, Time: 20 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,39 +1,22 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
-string arr[20000], tmp[20000];
-
+string arr[20000];
 bool cmp(const string &a, const string &b) {
     if(a.length() == b.length()) return a < b;
     return a.length() < b.length();
 }
 
-void mergeSort(int start, int end) {
-    if(end - start <= 1) return;
-    int mid = (start + end) / 2;
-    mergeSort(start, mid);
-    mergeSort(mid, end);
-    int left = start, right = mid;
-    for(int i = start; i < end; i++) {
-        if(left == mid) tmp[i] = arr[right++];
-        else if(right == end) tmp[i] = arr[left++];
-        else if(cmp(arr[left], arr[right])) tmp[i] = arr[left++];
-        else tmp[i] = arr[right++];
-    }
-    for(int i = start; i < end; i++) arr[i] = tmp[i];
-}
-
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int N; cin >> N;
-    for(int i = 0; i < N; i++) cin >> arr[i];
-    mergeSort(0, N);
-    int size = 1;
-    for(int i = 1; i < N; i++) {
-        if(arr[i] == arr[i-1]) continue;
-        if(size == i) size++;
-        else arr[size++] = arr[i];
-    }
-    for(int i = 0; i < size; i++) cout << arr[i] << '\n';
+    for(int i=0; i<N; i++) cin >> arr[i];
+    sort(arr, arr + N, cmp);
+
+    cout << arr[0] << '\n';
+    for(int i = 1; i < N; i++)
+        if(arr[i] != arr[i-1])
+            cout << arr[i] << '\n';
 }
