dce476cd5987800136f97d8e7e220a16224a070f
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-23 17:59:30 +0900

    [Silver V] Title: 단어 정렬, Time: 28 ms, Memory: 6840 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,29 +1,43 @@
 #include <iostream>
 #include <string>
-#include <unordered_set>
-#include <algorithm>
 using namespace std;
+
+string arr[20000], tmp[20000];
 
 bool cmp(const string &a, const string &b) {
     if(a.length() != b.length()) return a.length() < b.length();
-    for(int i=0; i<a.length(); i++) {
+    for(int i = 0; i < (int)a.length(); i++) {
         if(a[i] == b[i]) continue;
         return a[i] < b[i];
     }
     return true;
 }
 
+void mergeSort(int start, int end) {
+    if(end - start <= 1) return;
+    int mid = (start + end) / 2;
+    mergeSort(start, mid);
+    mergeSort(mid, end);
+    int left = start, right = mid;
+    for(int i = start; i < end; i++) {
+        if(left == mid) tmp[i] = arr[right++];
+        else if(right == end) tmp[i] = arr[left++];
+        else if(cmp(arr[left], arr[right])) tmp[i] = arr[left++];
+        else tmp[i] = arr[right++];
+    }
+    for(int i = start; i < end; i++) arr[i] = tmp[i];
+}
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int N; cin >> N;
-    unordered_set<string> W;
-    for(int i=0; i<N; i++) {
-        string s; cin >> s;
-        W.insert(s);
+    for(int i = 0; i < N; i++) cin >> arr[i];
+    mergeSort(0, N);
+    int size = 1;
+    for(int i = 1; i < N; i++) {
+        if(arr[i] == arr[i-1]) continue;
+        if(size == i) size++;
+        else arr[size++] = arr[i];
     }
-    int size = 0;
-    string words[W.size()];
-    for(const string &s : W) words[size++] = s;
-    sort(words, words+size, cmp);
-    for(const string &s : words) cout << s << '\n';
+    for(int i = 0; i < size; i++) cout << arr[i] << '\n';
 }
