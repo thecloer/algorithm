2bb93609e217d9e213d2036729fafed5bad2b62e
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-23 11:47:46 +0900

    [Gold III] Title: 드래곤 커브, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
@@ -1,52 +1,30 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-struct xy {int x, y;};
 bool board[101][101];
-xy dxy[] = {{1,0},{0,-1},{-1,0},{0,1}};
-
-inline void mark(const xy &pos) {
-    if(!board[pos.x][pos.y]) board[pos.x][pos.y] = true;
-}
-void copyClockwise90(vector<xy> &path) {
-    xy pin = path.back();
-    vector<xy> copy;
-    for(auto i = path.rbegin()+1; i != path.rend(); i++) {
-        int x = i->x - pin.x;
-        int y = i->y - pin.y;
-
-        int tmp = x;
-        x = -y;
-        y = tmp;
-
-        x += pin.x;
-        y += pin.y;
-
-        copy.push_back({x, y});
-        mark(copy.back());
-    }
-    for(const auto &v:copy) path.push_back(v);
-}
-void curve(vector<xy> &path, int d, int g) {
-    if(g == 0) {
-        path.push_back({path.back().x + dxy[d].x, path.back().y + dxy[d].y});
-        mark(path.back());
-        return;
-    }
-    curve(path, d, g-1);
-    copyClockwise90(path);
-}
+int curve[1024];
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int N; cin >> N;
     while(N--) {
         int x, y, d, g; cin >> x >> y >> d >> g;
-        vector<xy> path = {{x, y}};
-        mark(path.front());
-        curve(path, d, g);
+        board[x][y] = true;
+        int curveSize = 1;
+        curve[0] = d;
+        while(g--)
+            for(int i=curveSize-1; i>=0; i--)
+                curve[curveSize++] = (curve[i] + 1) & 3;
+        for(int i=0; i<curveSize; i++) {
+            int dir = curve[i];
+            if(dir == 0) x++;
+            else if(dir == 1) y--;
+            else if(dir == 2) x--;
+            else y++;
+            board[x][y] = true;
+        }
     }
+
     int ans = 0;
     for(int i=0; i<100; i++)
         for(int j=0; j<100; j++)
