7b4165b6b2c7773d5d782da82cfe093dbc2a7e91
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-27 13:52:04 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int sum[1001], num[1001];
+int num[1001], sum[1001];
 int max(int a, int b) {return a > b ? a : b;}
 
 int main() {
@@ -9,7 +9,7 @@ int main() {
     int N; cin >> N;
     for(int i=0; i<N; i++) cin >> num[i];
     for(int i=0; i<N; i++) {
-        for(int j=i-1; j>=0; j--)
+        for(int j=0; j<i; j++)
             if(num[j] < num[i])
                 sum[i] = max(sum[i], sum[j]);
         sum[i] += num[i];
