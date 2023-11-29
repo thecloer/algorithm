7d5e4a105d323a01e7e97fc8b4ea2df92b4442a4
thecloer <83699438+thecloer@users.noreply.github.com>
2023-11-30 06:25:37 +0900

    [Gold IV] Title: 이차원 배열과 연산, Time: 4 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.cc
+++ 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.cc
@@ -8,6 +8,13 @@ int R, C, K;
 int rowSize = 3, colSize = 3;
 int matrix[100][100];
 
+void transpose() {
+    int mx = max(rowSize, colSize);
+    for(int i=0; i<mx; i++)
+        for(int j=i+1; j<mx; j++)
+            swap(matrix[i][j], matrix[j][i]);
+    swap(rowSize, colSize);
+}
 void operateR() {
     int maxLen = 0;
     for(int row=0; row<rowSize; row++) {
@@ -32,30 +39,6 @@ void operateR() {
     }
     colSize = maxLen;
 }
-void operateC() {
-    int maxLen = 0;
-    for(int col=0; col<colSize; col++) {
-        unordered_map<int, int> freq;
-        for(int i = 0; i < rowSize; i++)
-            if(matrix[i][col])
-                freq[matrix[i][col]]++;
-        
-        vector<pair<int, int>> v;
-        for(const auto [n, f]:freq)
-            v.push_back({ f, n });
-        sort(v.begin(), v.end());
-
-        int len = 0;
-        for(const auto [f, n]:v) {
-            matrix[len++][col] = n;
-            matrix[len++][col] = f;
-        }
-
-        for(int i=len; i<rowSize; i++) matrix[i][col] = 0;
-        maxLen = max(maxLen, len);
-    }
-    rowSize = maxLen;
-}
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
@@ -64,11 +47,17 @@ int main() {
     for(int i=0; i<rowSize; i++)
         for(int j=0; j<colSize; j++)
             cin >> matrix[i][j];
-
+    
     int sec = 0;
     while(matrix[R][C] != K and ++sec <= 100) {
-        if(rowSize < colSize) operateC();
-        else operateR();
+        bool transposed = false;
+        if(rowSize < colSize) {
+            transpose();
+            transposed = true;
+        }
+        operateR();
+        
+        if(transposed) transpose();
     }
     cout << (sec > 100 ? -1 : sec);
 }
