4d8ffe5dd63258474cac75730e5ff4c1d4695476
thecloer <83699438+thecloer@users.noreply.github.com>
2023-12-04 00:25:32 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 76 ms, Memory: 5744 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+const int MAX_DIST = 0x3f3f3f3f;
+
+struct vw {int v, w;};
+int N, E;
+int dist[801];
+vector<vw> adj[801];
+
+bool cmp(vw &a, vw &b) {return a.w > b.w;}
+void dijkstra(int start) {
+    for(int i=1; i<=N; i++) dist[i] = MAX_DIST;
+    dist[start] = 0;
+    priority_queue<vw, vector<vw>, decltype(&cmp)> Q(cmp);
+    Q.push({start, 0});
+    while(not Q.empty()) {
+        const auto [x, d] = Q.top(); Q.pop();
+        for(const auto [nx, w]:adj[x]) {
+            if(dist[nx] > dist[x] + w) {
+                dist[nx] = dist[x] + w;
+                Q.push({nx, dist[nx]});
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N >> E;
+    for(int i=0; i<E; i++) {
+        int a, b, c; cin >> a >> b >> c;
+        adj[a].push_back({b, c});
+        adj[b].push_back({a, c});
+    }
+    int v1, v2; cin >> v1 >> v2;
+    dijkstra(1);
+    int path1 = dist[v1];
+    int path2 = dist[v2];
+    dijkstra(N);
+    path1 += dist[v2];
+    path2 += dist[v1];
+
+    int ans = path1 < path2 ? path1 : path2;
+    if(ans < MAX_DIST) {
+        dijkstra(v1);
+        ans += dist[v2];
+    }
+    cout << (ans < MAX_DIST ? ans : -1);
+}
