f5fdb44c09c1e5f67254b730bcabd6695eb39236
thecloer <83699438+thecloer@users.noreply.github.com>
2023-12-05 23:13:59 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -6,10 +6,8 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     int N; cin >> N;
     pair<int, int> task[N];
-    for(int i=0; i<N; i++) {
-        int s, e; cin >> s >> e;
-        task[i] = {e, s};
-    }
+    for(int i=0; i<N; i++)
+        cin >> task[i].second >> task[i].first;
     sort(task, task+N);
     int ans = 0, t = 0;
     for(const auto &[e, s]:task) {
