99d022222b68f413ab544f944d7cec267f93b35e
thecloer <83699438+thecloer@users.noreply.github.com>
2023-12-05 13:48:08 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 48 ms, Memory: 6664 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -3,12 +3,12 @@
 #include <vector>
 using namespace std;
 
-struct edge {int v, w;};
-int V, E, len, cnt;
+struct xw {int x, w;};
+int V, E;
 bool vis[10001];
-vector<edge> adj[10001];
+vector<xw> adj[10001];
 
-bool cmp(edge &a, edge &b) {return a.w > b.w;}
+bool cmp(xw &a, xw &b) {return a.w > b.w;}
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
@@ -19,18 +19,19 @@ int main() {
         adj[b].push_back({a, c});
     }
 
-    priority_queue<edge, vector<edge>, decltype(&cmp)> pq(cmp);
+    priority_queue<xw, vector<xw>, decltype(&cmp)> pq(cmp);
     pq.push({1, 0});
+
+    int len = 0, cnt = 0;
     while(!pq.empty()) {
         const auto [x, w] = pq.top(); pq.pop();
         if(vis[x]) continue;
         vis[x] = true;
         len += w;
         if(++cnt == V) break;
-        for(const auto [nx, nw]:adj[x]) {
-            if(vis[nx]) continue;
-            pq.push({nx, nw});
-        }
+        for(const auto [nx, nw]:adj[x])
+            if(not vis[nx]) 
+                pq.push({nx, nw});
     }
     cout << len;
 }
