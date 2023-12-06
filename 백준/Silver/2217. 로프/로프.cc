080fca4f4ba7374658996eed8d49847ef76bc12b
thecloer <83699438+thecloer@users.noreply.github.com>
2023-12-06 09:09:31 +0900

    [Silver IV] Title: 로프, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -1,14 +1,21 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
+inline int max(int a, int b) {return a > b ? a : b;}
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    int N; cin >> N;
-    long long rope[N];
-    for(int i=0; i<N; i++) cin >> rope[i];
-    sort(rope, rope+N);
-    long long ans = 0;
-    for(int i=0; i<N; i++) ans = max(ans, rope[i] * (N-i));
+    int N, x; cin >> N;
+    int freq[10001]{};
+    for(int i=0; i<N; i++) {
+        cin >> x;
+        freq[x]++;
+    }
+    
+    int ans = 0, acc = 0;
+    for(int len=1; len<10001; len++) {
+        if(not freq[len]) continue;
+        ans = max(ans, len * (N - acc));
+        acc += freq[len];
+    }
     cout << ans;
 }
