7ece40413f5150890644d12d6b2587c1de44e6dc
thecloer <83699438+thecloer@users.noreply.github.com>
2023-12-10 00:30:52 +0900

    [Gold III] Title: 파티, Time: 176 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -2,26 +2,27 @@
 #include <vector>
 #include <queue>
 using namespace std;
-const int MAX_DIST = 1000001;
+const int MAX_DIST = 100000;
 
-struct node {int v, w;};
+struct xd {int x, d;};
 int N, M, X;
-vector<node> graph[1001];
+vector<xd> adj[1001];
 int dist[1001];
 
-bool cmp(int a, int b) { return dist[a] > dist[b]; }
+bool cmp(const xd &a, const xd &b) { return a.d > b.d; }
 int max(int a, int b) { return a > b ? a : b; }
 int dijkstra(int from, int to) {
+    priority_queue<xd, vector<xd>, decltype(&cmp)> pq(cmp);
     for(int i=1; i<=N; i++) dist[i] = MAX_DIST;
     dist[from] = 0;
-    priority_queue<int, vector<int>, decltype(&cmp)> pq(cmp);
-    pq.push(from);
+    pq.push({from, 0});
     while(!pq.empty()) {
-        int x = pq.top(); pq.pop();
-        for(const auto &[nx, w] : graph[x]) {
-            if(dist[nx] <= dist[x] + w) continue;
-            dist[nx] = dist[x] + w;
-            pq.push(nx);
+        const auto [x, d] = pq.top(); pq.pop();
+        if(d > dist[x]) continue;
+        for(const auto &[nx, nw] : adj[x]) {
+            if(dist[nx] <= dist[x] + nw) continue;
+            dist[nx] = dist[x] + nw;
+            pq.push({nx, dist[nx]});
         }
     }
     return dist[to];
@@ -31,7 +32,7 @@ int main() {
     cin >> N >> M >> X;
     while(M--) {
         int from, to, w; cin >> from >> to >> w;
-        graph[from].push_back({to, w});
+        adj[from].push_back({to, w});
     }
     int ans = 0;
     for(int start = 1; start <= N; start++)
