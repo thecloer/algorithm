884e0c16400c94c0658a8300f2fbb40498c6edfa
thecloer <83699438+thecloer@users.noreply.github.com>
2023-12-10 23:26:58 +0900

    [Gold IV] Title: 알파벳, Time: 216 ms, Memory: 31616 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,22 +1,23 @@
 #include <iostream>
+#include <set>
 using namespace std;
 
 struct xy {int x, y;};
 int R, C, ans;
-char board[21][21];
-bool vis[26];
+int board[21][21];
+set<int> vis[21][21];
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
 
-void backTracking(int x, int y, int cnt) {
+void backTracking(int x, int y, int cnt, int bit) {
+    bit |= 1 << board[x][y];
+    if(not vis[x][y].insert(bit).second) return;
     bool isLeaf = true;
     for(int dir = 4; dir--;) {
         int nx = x + dx[dir], ny = y + dy[dir];
-        if(nx < 1 or nx > R or ny < 1 or ny > C) continue;
-        if(vis[board[nx][ny] - 'A']) continue;
-        vis[board[nx][ny] - 'A'] = true;
-        backTracking(nx, ny, cnt+1);
-        vis[board[nx][ny] - 'A'] = false;
+        if(nx < 0 or nx >= R or ny < 0 or ny >= C) continue;
+        if(bit & (1 << board[nx][ny])) continue;
+        backTracking(nx, ny, cnt+1, bit);
 
         if(isLeaf) isLeaf = false;
     }
@@ -26,11 +27,13 @@ void backTracking(int x, int y, int cnt) {
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> R >> C;
-    for(int i=1; i<=R; i++)
-        for(int j=1; j<=C; j++)
-            cin >> board[i][j];
+    for(int i=0; i<R; i++) {
+        for(int j=0; j<C; j++) {
+            char c; cin >> c;
+            board[i][j] = c - 'A';
+        }
+    }
     
-    vis[board[1][1] - 'A'] = true;
-    backTracking(1, 1, 1);
+    backTracking(0, 0, 1, 0);
     cout << ans;
 }
