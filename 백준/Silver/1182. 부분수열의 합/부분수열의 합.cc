184a0cb93901e80196d3ca4683c8e6f0e6c5b049
thecloer <83699438+thecloer@users.noreply.github.com>
2023-12-19 23:06:42 +0900

    [Silver II] Title: 부분수열의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -2,23 +2,22 @@
 using namespace std;
 
 int N, S, ans;
-int arr[20];
-bool used[20];
+int num[40];
 
-void backtracking(int cur, int sum) {
+void backTracking(int cur, int acc) {
     if(cur == N) {
-        if(sum == S) ans++;
+        if(acc == S) ans++;
         return;
     }
-    backtracking(cur+1, sum);
-    backtracking(cur+1, sum + arr[cur]);
+    backTracking(cur+1, acc);
+    backTracking(cur+1, acc + num[cur]);
 }
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> N >> S;
-    for(int i=0; i<N; i++) cin >> arr[i];
-    backtracking(0, 0);
+    for(int i=0; i<N; i++) cin >> num[i];
+    backTracking(0, 0);
     if(S == 0) ans--;
     cout << ans;
 }
