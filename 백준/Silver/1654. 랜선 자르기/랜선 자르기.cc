c58e27ae6c05c7baf774d05ff60589f0bc1b3ea4
thecloer <83699438+thecloer@users.noreply.github.com>
2024-03-20 19:33:42 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -3,33 +3,27 @@
 using namespace std;
 
 int K, N;
-ll wires[10000];
-
-ll cut(ll len) {
-    ll cnt = 0;
-    for(int i=0; i<K; i++) {
-        if(wires[i] < len) continue;
-        cnt += wires[i] / len;
-    }
-    return cnt;
-}
+int wires[10000];
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     cin >> K >> N;
-    ll mn = 0, mx = 0;
+    ll s = 0, e = 0, m = 0;
     for(int i=0; i<K; i++) {
         cin >> wires[i];
-        if(wires[i] > mx) mx = wires[i];
+        if(wires[i] > e) e = wires[i];
     }
     
-    ll len;
-    while(mn < mx) {
-        len = (mn + mx) >> 1;
-        if(len == mn) break;
-        if(cut(len) < N) mx = len;
-        else mn = len;
+    while(s < e) {
+        m = (s + e) >> 1;
+        if(s == m) m++;
+
+        ll cnt = 0;
+        for(int i=0; i<K; i++) cnt += wires[i] / m;
+
+        if(cnt < N) e = m - 1;
+        else s = m;
     }
-    if(cut(len + 1) < N) cout << len;
-    else cout << len + 1;
+    cout << s;
 }
+
