ce5e4d6c50f19515a88e4cf24010c789c8b22854
thecloer <83699438+thecloer@users.noreply.github.com>
2024-04-01 16:05:32 +0900

    [Gold I] Title: 비숍, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1799. 비숍/비숍.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+enum color {BLACK, WHITE};
+struct xy {int x, y;};
+
+int n, ans[2];
+vector<xy> B[2];
+bool diag[2][2][20]; // [color][0]: +, [color][1]: -
+
+void backtracking(color c, int cur, int cnt) {
+    if(cur == B[c].size()) {
+        if(cnt > ans[c]) ans[c] = cnt;
+        return;
+    }
+    // if(cnt + B[c].size() - cur < ans[c]) return;
+
+    if(diag[c][0][B[c][cur].x + B[c][cur].y] or diag[c][1][B[c][cur].x - B[c][cur].y + n]) {
+        backtracking(c, cur+1, cnt);
+        return;
+    }
+
+    diag[c][0][B[c][cur].x + B[c][cur].y] = diag[c][1][B[c][cur].x - B[c][cur].y + n] = true;
+    backtracking(c, cur+1, cnt+1);
+    diag[c][0][B[c][cur].x + B[c][cur].y] = diag[c][1][B[c][cur].x - B[c][cur].y + n] = false;
+    backtracking(c, cur+1, cnt);
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> n;
+    bool isB;
+    for(int x=0; x<n; x++) {
+        for(int y=0; y<n; y++) {
+            cin >> isB;
+            if(isB) B[(x ^ y) & 1].push_back({x, y});
+        }
+    }
+    
+    backtracking(BLACK, 0, 0);
+    backtracking(WHITE, 0, 0);
+    cout << ans[BLACK] + ans[WHITE];
+}
